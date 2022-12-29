ebe965b2d23e1caf95c9cf2ed976a6c730fcfa38
ntuan2502 <ntuan.2502@gmail.com>
2022-12-29 14:15:07 +0700

    first commit

---
+++ app/Models/Product.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
+
+class Product extends Model
+{
+    use HasApiTokens;
+    use HasFactory;
+
+    protected $fillable = ['name', 'price'];
+}
